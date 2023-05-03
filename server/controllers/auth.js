e695f494724ff5003a5206f94caeb7e26065871b
Sajib Saha <sajibsaha859@gmail.com>
2023-05-04 04:23:39 +0530

    authentication

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,19 +3,35 @@ const { hashPassword, comparePassword } = require("../helpers/auth");
 const data = async (req, res) => {
   // console.log("Register endpoint=>", req.body);
 
-  let { name, email, password, question, secret } = req.body;
+  let { name, email, password, confirmPassword, question, secret } = req.body;
   const nameC = name.replaceAll(/\s+/g, " ");
   const secretC = secret.replaceAll(/\s+/g, " ");
+  const passC = password.replaceAll(/\s+/g, "");
 
   //validation
   if (!nameC || nameC == " ") return res.status(400).send("Name is required");
   if (!email) return res.status(400).send("Email is required");
-  if (!password || password.length < 8)
+  if (!passC || passC.length < 8)
     return res
       .status(400)
       .send(
         "Password is required and should be minimum 8 characters & maximum 64 characters long"
       );
+  const s1 = "Hello";
+  const s2 = "Hello2";
+  console.log(password !== confirmPassword);
+  const match = (password, confirmPassword) => {
+    if (password !== confirmPassword) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+  console.log(typeof password);
+
+  if (password !== confirmPassword)
+    return res.status(400).send("Passwords don't match");
+
   if (!question) return res.status(400).send("Select a question");
   if (!secretC || secretC == " ")
     return res.status(400).send("Answer is required");
