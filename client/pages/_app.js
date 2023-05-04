922f0be0900355f992eda53e803cdf9f70c95151
Sajib Saha <sajibsaha859@gmail.com>
2023-05-05 03:37:55 +0530

    Added UserProvider

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -5,10 +5,11 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import "antd/dist/reset.css";
 import "../public/css/style.css";
+import { UserProvider } from "../context";
 import MyNavbar from "../components/nav2";
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserProvider>
       <Head>
         <link rel="stylesheet" href="/css/style.css" />
       </Head>
@@ -26,7 +27,7 @@ function MyApp({ Component, pageProps }) {
         theme="dark"
       />
       <Component {...pageProps} />
-    </>
+    </UserProvider>
   );
 }
 export default MyApp;
