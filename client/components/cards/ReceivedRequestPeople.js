715a968050fe05b351c0c8b69b8a53543c5500d8
Sajib Saha <sajibsaha859@gmail.com>
2023-05-29 22:09:50 +0530

    array temporary fix

--- client/components/cards/ReceivedRequestPeople.js
+++ client/components/cards/ReceivedRequestPeople.js
@@ -9,9 +9,13 @@ import axios from "axios";
 const People = ({ people, setPeople, total }) => {
   const [state, setState] = useContext(UserContext);
   const id = state.user._id;
-  const length = state.user.followers.length;
+  const [length, setLength] = useState(state.user.followers.length);
 
-  const [add2, setAdd2] = useState(Array(length).fill(false));
+  useEffect(() => {
+    if (state && state.user && state.user.followers) {
+      setLength(state.user.followers.length);
+    }
+  }, [state, setLength]);
 
   const router = useRouter();
   const handleConfirm = async (single, index) => {
@@ -22,6 +26,7 @@ const People = ({ people, setPeople, total }) => {
         _id: single._id,
         id,
       });
+
       const setAddValue = (index, value) => {
         setAdd2((prevAdd) => {
           const updatedAdd = [...prevAdd];
@@ -70,71 +75,78 @@ const People = ({ people, setPeople, total }) => {
       toast.error(err);
     }
   };
-  return (
-    <pre>
-      {people &&
-        people.map((single, index) => (
-          <div key={single._id} className="card mb-2" style={{ width: "100%" }}>
-            <div className="card-header head align-item-center justify-content-between follow">
-              <div className="row d-flex bg-dark">
-                <div className="col-2 text-light">
-                  {single.photo ? (
-                    <Avatar
-                      src={single.photo}
-                      size={40}
-                      className="mt-1 ms-1 p-1"
-                    />
-                  ) : (
-                    <Avatar size={40} className="mt-1 ms-1 ">
-                      {single.name.charAt(0)}
-                    </Avatar>
-                  )}
+  const [add2, setAdd2] = useState(Array(1000).fill(false));
+  if (state && state.user) {
+    return (
+      <pre>
+        {people &&
+          people.map((single, index) => (
+            <div
+              key={single._id}
+              className="card mb-2"
+              style={{ width: "100%" }}
+            >
+              <div className="card-header head align-item-center justify-content-between follow">
+                <div className="row d-flex bg-dark">
+                  <div className="col-2 text-light">
+                    {single.photo ? (
+                      <Avatar
+                        src={single.photo}
+                        size={40}
+                        className="mt-1 ms-1 p-1"
+                      />
+                    ) : (
+                      <Avatar size={40} className="mt-1 ms-1 ">
+                        {single.name.charAt(0)}
+                      </Avatar>
+                    )}
+                  </div>
+                  <div
+                    className="col-6 col-md-10 mt-1  text-light "
+                    style={{ fontSize: "25px" }}
+                  >
+                    {single.name}
+                  </div>
+                </div>
+              </div>
+              <div className="carb-body overflow-hidden">
+                <div className="row d-flex bg-dark" hidden={add2[index]}>
+                  <div className="col-6 jusify-content-between text-light">
+                    <button
+                      className="btn btn-primary btn-lg text-light ms-4"
+                      onClick={() => handleConfirm(single, index)}
+                    >
+                      Confirm
+                    </button>
+                  </div>
+                  <div
+                    className="col-6 text-light jusify-content-between"
+                    style={{ fontSize: "25px" }}
+                  >
+                    <button
+                      className="btn btn-danger btn-lg text-light"
+                      onClick={() => handleDelete(single, index)}
+                    >
+                      Delete
+                    </button>
+                  </div>
                 </div>
                 <div
-                  className="col-6 col-md-10 mt-1  text-light "
-                  style={{ fontSize: "25px" }}
+                  className="row d-flex bg-primary text-light"
+                  hidden={!add2[index]}
                 >
-                  {single.name}
+                  <h3>Request Accepted</h3>
                 </div>
               </div>
             </div>
-            <div className="carb-body overflow-hidden">
-              <div className="row d-flex bg-dark" hidden={add2[index]}>
-                <div className="col-6 jusify-content-between text-light">
-                  <button
-                    className="btn btn-primary btn-lg text-light ms-4"
-                    onClick={() => handleConfirm(single, index)}
-                  >
-                    Confirm
-                  </button>
-                </div>
-                <div
-                  className="col-6 text-light jusify-content-between"
-                  style={{ fontSize: "25px" }}
-                >
-                  <button
-                    className="btn btn-danger btn-lg text-light"
-                    onClick={() => handleDelete(single, index)}
-                  >
-                    Delete
-                  </button>
-                </div>
-              </div>
-              <div
-                className="row d-flex bg-primary text-light"
-                hidden={!add2[index]}
-              >
-                <h3>Request Accepted</h3>
-              </div>
-            </div>
+          ))}
+        {!people && (
+          <div>
+            <h3 className="text-primary">No Request Received</h3>
           </div>
-        ))}
-      {!people && (
-        <div>
-          <h3 className="text-primary">No Request Received</h3>
-        </div>
-      )}
-    </pre>
-  );
+        )}
+      </pre>
+    );
+  }
 };
 export default People;
